bbf00406a26e293c140d0142c6331cd34c67ef85
HanPhi20 <jenga5566@gmail.com>
2023-08-17 11:31:13 +0700

    update

---
+++ deMo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    float x, a, b, c, delta
+    printf("input int a any: ");
+    scanf("%d, &a");
+    printf("input int b any: ");
+    scanf("%d, &b");
+    printf("input any c any: ");
+    scanf("%d, &c");
+
+
+}
